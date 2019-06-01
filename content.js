465e7de3e05047b8cc51b5a0113b007077d46015
danizat <dbaiyshova@gmail.com>
2019-06-02 02:46:20 +0600

    BIG CHANGES

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", () => {
-    chrome.storage.local.get(['openCommand'], function(result) {
-        if (result.openCommand === "openModal") {
+    function checkStorage() {
+        chrome.storage.local.get(['openCommand'], function(result) {
+
+            if (result.openCommand === "openModal") {
             // let modalContainer = document.createElement("modalContainer");
             modalContainer.style.width = "100%";
             modalContainer.style.height = "100%";
@@ -14,33 +16,60 @@ window.addEventListener("load", () => {
 
             console.log("createElement");
 
-            let appender = `<div class = "fakeBox"><div class="
-                flexslider"><ul class="
-                slides"><li><div><button class = "stopBtn">Stop</button></div></li><li><div>2</div></li><li><div>3</div></li></ul></div></div>`
+            let appender = `<div class = "modalBox">
+                                <div class="flexslider">
+                                
+                                    <ul class="slides">
+                                        <li><div><img src = "https://cdn.dribbble.com/users/1056629/screenshots/6459209/dribbble_fireart_4.gif"></div></li>
+                                        <li><div><button class = "closeBtn">Close</button></div></li>
+                                        <li><div><img src = "https://cdn.dribbble.com/users/1056629/screenshots/6464387/dribbble_fireart_5.gif"></div></li>
+                                        <li><div><img src ="https://cdn.dribbble.com/users/1056629/screenshots/6553608/dribbble_fireart_7.gif"></div></li>
+                                        <li><div><img src = "https://cdn.dribbble.com/users/1056629/screenshots/6475612/dribbble_fireart_3.gif"></div></li>
+                                        <li><div><audio controls autoplay ="true" src="http://www.tunescoop.com/play/343933323930/khalid-normani-love-lies-mp3" type="audio/mpeg">
+ 
+  <p>Your browser doesn't support HTML5 audio. Here is
+     a <a href="http://www.tunescoop.com/play/343933323930/khalid-normani-love-lies-mp3">link to the audio</a> instead.</p>
+</audio></div></li>
+                                        <li><div>7</div></li>
+                                        <li><div>8</div></li>
+                                    </ul>
+                                </div>
+                            </div>`
 
 
             $(modalContainer).append(appender);
             $("body").append(modalContainer);
-
             $(modalContainer).ready(
-                $('.flexslider').flexslider({
-                    animation: "slide"
-                })     
+                
+                    $('.flexslider').flexslider({animation: "slide",slideshowSpeed: 5000,animationSpeed: 800, }) 
+                                   
             );
-             $(".stopBtn").click($('#slider').flexslider("stop"))
+            }
+            $(".closeBtn").click(function(e) {
+                e.preventDefault();
+                $(".modalContainer").addClass("dNone");
+                $(clearStorage());
+            }) ;
+            // $(".closeBtn").click($(console.log("watswrong"))) ;
+            // $(".closeBtn").click($(clearStorage())) ;
+            // $(".closeBtn").click($(checkStorage())) ;
+            // $(".closeBtn").click($(".modalContainer").remove()) ;
+           
+        });
 
-            // div.addEventListener("click", () => {()
+    }
 
-            // chrome.storage.local.set({
-            //             key: ""
-            //         }, function() {
-            //             console.log("Value is empty now");
+    checkStorage();
+    
 
-            // })
-            // })
+function clearStorage() {
+            chrome.storage.local
+            .set({  key: ""},
+                    function() { console.log("Value is empty now");})
+}
 
+    
+    
+ 
 
-
-        }
-    });
 })
